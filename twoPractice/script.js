f9a46cdbd62c4b6f7a37e5629c5393ca44dddc4a
Nikolay <Kolianssssss2014@mail.ru>
2022-07-03 15:08:29 +0300

    commit

--- twoPractice/script.js
+++ twoPractice/script.js
@@ -4,3 +4,16 @@ for (let i = 0; i < input.length; i++) {
     output += input[i].replace('a','4').replace('e','3').replace('i','1');
 }
 console.log(output);
+
+let str = 'move';
+const reverseString = () => {
+    let result = 'lol - ';
+    let i;
+    for ( i = 0; i < str.length; i+=2) {
+      result = `${result}${str[i+1]}${str[i]}`;
+    }
+  
+    return result;
+  };
+
+console.log(reverseString())
