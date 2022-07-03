f9c38c2568a588a8214ccd1d1d0637685fb17a66
Nikolay <Kolianssssss2014@mail.ru>
2022-07-03 15:28:33 +0300

    commit

--- twoPractice/script.js
+++ twoPractice/script.js
@@ -1,3 +1,5 @@
+///Зашифрованная строка
+
 let input = 'javascript is awesome';
 let output = "";
 for (let i = 0; i < input.length; i++) {
@@ -17,3 +19,41 @@ const reverseString = () => {
   };
 
 console.log(reverseString())
+
+//Дан массив с числами.
+//Числа могут быть положительными и отрицательными. 
+///Найдите сумму положительных элементов массива.
+
+function randomNumber(min, max){
+  const r = Math.random()*(max-min) + min
+  return Math.floor(r)
+}
+let arr = [];
+let mass = [];
+for (let i = 0, el = 10; i < el; i++) {
+  arr.push(randomNumber(-10,10));
+  mass.push(arr[i]);
+}
+console.log(mass);
+let sum = 0;
+for(let i = 0, el = 10; i < el; i++){
+  if(mass[i]>=0){
+    sum += mass[i];
+  }
+}
+console.log('Сумма положительных элементов массива: ' + sum);
+
+// Дан массив числами, например: [10, 20, 30, 50, 235, 3000].
+// Выведите на экран только те числа из массива, которые начинаются на цифру 1, 2 или 5.
+
+let array = [10,20,30,50,235,3000];
+let massiv =[];
+let resultMassiv = [];
+massiv = array.join().split(',');
+console.log(massiv);
+for (let i = 0; i < massiv.length; i++) {
+  if(String(massiv[i]).startsWith(1)||String(massiv[i]).startsWith(2)||String(massiv[i]).startsWith(5)){
+    resultMassiv.push(massiv[i])
+  }
+}
+console.log(resultMassiv);
