a3ef6242bbc43b5d0f27296458c41d6dafb6c9fc
Nikolay <Kolianssssss2014@mail.ru>
2022-08-17 14:21:18 +0300

    Тест для функции по поиску значения в объекте по ключу

---
+++ Automatic_Testing/1.Practice/get.js
@@ -0,0 +1,12 @@
+// Напишите тесты для функции get(obj, key, defaultValue). 
+// Эта функция извлекает значение из объекта при условии, что ключ существует. 
+// В ином случае возвращается defaultValue.
+
+
+let get = (obj,val,defaultValue) => {
+    return _.get(obj, val,defaultValue);
+}
+
+console.log(get({ hello: 'world' }, 'hello')); // world
+console.log(get({ hello: 'world' }, 'hello', 'kitty')); // 'world'
+console.log(get({}, 'hello', 'kitty'));// 'kitty'
