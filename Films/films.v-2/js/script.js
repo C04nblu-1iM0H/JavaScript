d3c368e0737db30ca5c8f9698744320e65750a51
Nikolay <Koliansssss2014@mail.ru>
2022-10-15 16:13:24 +0300

    films v2

---
+++ Films/films.v-2/js/script.js
@@ -0,0 +1,57 @@
+/* Задания на урок:
+1) Удалить все рекламные блоки со страницы (правая часть сайта) yes
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+3) Изменить задний фон с постером фильма на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+5) Добавить нумерацию выведенных фильмов */
+
+
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+
+const boxAdvertisement = document.querySelector('.promo__adv'),                   //присваиваю переменной элемент со страницы под классам .promo__adv
+    boxAdvertisementTitle = boxAdvertisement.querySelector('.promo__adv-title'),//присваиваю переменной элемент со страницы под классам .promo__adv-title
+    boxAdvertisementList = boxAdvertisement.querySelectorAll('img'),            //присваиваю переменной HTMLCollection со страницы под классам .promo__adv
+    boxInfoAboutCinemo = document.querySelector('.promo__bg'),
+    genre = boxInfoAboutCinemo.querySelector('.promo__genre'),
+    movieList = document.querySelector('.promo__interactive-list');
+
+boxAdvertisementTitle.remove();             // удаляю заголовок при помощи функции remove()   
+boxAdvertisementList.forEach(item =>{       // через цикл forEach удаляю все рекламные баннеры с сайта
+    item.remove();                          //item - это каждый элемент в HTMLCollection 
+});
+
+genre.textContent = 'драма';                // переприсваиваю значение которое находится в теге div с классом '.promo__genre'
+// const div = document.createElement('div');  //создаю элемент, пока что только в js
+// div.textContent = 'драма';                 //присваиваю этому елементу какой-то текст 
+// div.classList.add('promo__genre');         //добовляю элементу div класс promo__genre
+// infoAboutCinemo[0].replaceWith(div);      //заменяем элемент с нулевым индексом (так как он находится в HTMLCollection) на новый элемент div 
+
+boxInfoAboutCinemo.style.cssText = 'background-image: url(./img/bg.jpg)';
+
+movieList.innerHTML = ""; 
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i+1} ${film}
+            <div class="delete"></div>
+        </li>`;
+});
+
+
+
+
+
